45470d67766d1380c8655896d2c2b9f7fb2d5891
jennie <beersoup@hotmail.com>
2017-04-24 21:35:53 +0200

    Text

--- src/components/banner.js
+++ src/components/banner.js
@@ -4,7 +4,7 @@ import BannerImage from './banner-image'
 
 const content = {
     mainHeader: "WHY?",
-    secondHeader: "Create fun and interactive Web Site",
+    secondHeader: "Create fun and interactive Web Sites",
     mainText: "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing.",
     bannerImageBg: "style/images/header-background.jpg"
 }
